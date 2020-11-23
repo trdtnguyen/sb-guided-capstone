26510d7659ae49b637f6c88f09ac3fd5f3c940f1
trdtnguyen <trauconnguyen@gmail.com>
2020-11-23 13:00:15 -0600

    implement digestion stage

---
+++ controllers/Extract.py
@@ -0,0 +1,181 @@
+"""
+Extract data sources
+"""
+__version__ = '0.1'
+__author__ = 'Dat Nguyen'
+
+from pyspark.sql import SparkSession
+from models.CommonEvent import CommonEvent
+from datetime import datetime
+import json
+"""
+parse the CSV line to CommonEvent object
+format of CSV line:
+    Trade:
+    TradeDate,RecordType,Symbol,ExecutionID,EventTime, Event Sequence Number,Exchange,Trade Price, Trade Size
+    
+    Quote:
+    TradeDate, RecordType, Symbol,EventTime, Event Sequence Number,Exchange, BidPrice, BidSize, AskPrice, AskSize  
+"""
+
+
+def parse_csv(line: str) -> CommonEvent:
+    record_type_pos = 1
+    record = line.split(',')
+    try:
+
+        if record[record_type_pos] == 'T':
+            # TradeDate,RecordType,Symbol,ExecutionID,EventTime, Event Sequence Number,Exchange,Trade Price, Trade Size
+            partition = 'T'
+            trade_dt = datetime.strptime(record[0], '%Y-%m-%d')
+            rec_type = record[1]
+            symbol = record[2]
+            event_time = datetime.strptime(record[4], '%Y-%m-%d')
+            event_seq_num = int(record[5])
+            exchange = record[6]
+            arrival_time = datetime.now()
+            trade_price = float(record[7])
+            trade_size = int(record[8])
+            bid_price = 0.0
+            bid_size = 0
+            ask_price = 0.0
+            ask_size = 0
+            line = ''
+
+            event = CommonEvent(partition, trade_dt, rec_type, symbol, event_time, event_seq_num, exchange,
+                                arrival_time,
+                                trade_price, trade_size, bid_price, bid_size, ask_price, ask_size,
+                                line)
+            return event
+        elif record[record_type_pos] == 'Q':
+            # TradeDate, RecordType, Symbol,EventTime, Event Sequence Number,Exchange, BidPrice, BidSize, AskPrice,
+            # AskSize
+            partition = 'Q'
+            trade_dt = datetime.strptime(record[0], '%Y-%m-%d')
+            rec_type = record[1]
+            symbol = record[2]
+            event_time = datetime.strptime(record[3], '%Y-%m-%d')
+            event_seq_num = int(record[4])
+            exchange = record[5]
+            arrival_time = datetime.now()
+            trade_price = 0.0
+            trade_size = 0
+            bid_price = float(record[6])
+            bid_size = int(record[7])
+            ask_price = float(record[8])
+            ask_size = int(record[9])
+            line = ''
+
+            event = CommonEvent(partition, trade_dt, rec_type, symbol, event_time, event_seq_num, exchange,
+                                arrival_time,
+                                trade_price, trade_size, bid_price, bid_size, ask_price, ask_size,
+                                line)
+            return event
+        else:
+            raise Exception
+    except Exception as e:
+        event = CommonEvent()
+        event.partition = 'B'
+        event.original_line = line
+        return event
+
+
+"""
+parse the JSON line to CommonEvent object
+format of JSON line:
+    Trade:
+    TradeDate,RecordType,Symbol,ExecutionID,EventTime, Event Sequence Number,Exchange,Trade Price, Trade Size
+
+    Quote:
+    TradeDate, RecordType, Symbol,EventTime, Event Sequence Number,Exchange, BidPrice, BidSize, AskPrice, AskSize  
+"""
+
+
+def parse_json(line: str) -> CommonEvent:
+    record_type_pos = 1
+    record = json.loads(line)
+
+    try:
+
+        if record['record_type'] == 'T':
+            # TradeDate,RecordType,Symbol,ExecutionID,EventTime, Event Sequence Number,Exchange,Trade Price, Trade Size
+            name_index = ['trade_date', 'record_type', 'symbol', 'executionid', "event_time",
+                          'event_seq_num', 'exchange', 'trade_price', 'trade_size']
+            partition = 'T'
+            trade_dt = datetime.strptime(record[name_index[0]], '%Y-%m-%d')
+            rec_type = record[name_index[1]]
+            symbol = record[name_index[2]]
+            event_time = datetime.strptime(record[name_index[4]], '%Y-%m-%d')
+            event_seq_num = int(record[name_index[5]])
+            exchange = record[name_index[6]]
+            arrival_time = datetime.now()
+            trade_price = float(record[name_index[7]])
+            trade_size = int(record[name_index[8]])
+            bid_price = 0.0
+            bid_size = 0
+            ask_price = 0.0
+            ask_size = 0
+            line = ''
+
+            event = CommonEvent(partition, trade_dt, rec_type, symbol, event_time, event_seq_num, exchange,
+                                arrival_time,
+                                trade_price, trade_size, bid_price, bid_size, ask_price, ask_size,
+                                line)
+            return event
+        elif record[record_type_pos] == 'Q':
+            # TradeDate, RecordType, Symbol,EventTime, Event Sequence Number,Exchange, BidPrice, BidSize, AskPrice,
+            # AskSize
+            name_index = ['trade_date', 'record_type', 'symbol', "event_time",
+                          'event_seq_num', 'exchange', 'bid_price', 'bid_size', 'ask_price', 'ask_size']
+            partition = 'Q'
+            trade_dt = datetime.strptime(record[name_index[0]], '%Y-%m-%d')
+            rec_type = record[name_index[1]]
+            symbol = record[name_index[2]]
+            event_time = datetime.strptime(record[name_index[3]], '%Y-%m-%d')
+            event_seq_num = int(record[name_index[4]])
+            exchange = record[name_index[5]]
+            arrival_time = datetime.now()
+            trade_price = 0.0
+            trade_size = 0
+            bid_price = float(record[name_index[6]])
+            bid_size = int(record[name_index[7]])
+            ask_price = float(record[name_index[8]])
+            ask_size = int(record[name_index[9]])
+            line = ''
+
+            event = CommonEvent(partition, trade_dt, rec_type, symbol, event_time, event_seq_num, exchange,
+                                arrival_time,
+                                trade_price, trade_size, bid_price, bid_size, ask_price, ask_size,
+                                line)
+            return event
+        else:
+            raise Exception
+    except Exception as e:
+        event = CommonEvent()
+        event.partition = 'B'
+        event.original_line = line
+        return event
+
+
+class Extract:
+    def __init__(self):
+        self.spark = SparkSession \
+            .builder \
+            .master('local') \
+            .appName('Equity Market Data Analysis') \
+            .getOrCreate()
+
+    """Extract data from data source
+    filepath: path to the data file. If the file is on Azure store, the file path is:
+    wasbs://<container-name>@<storage-account-name>.blob.core.windows.net/<path_in_container>"
+    parse_f: function for parse data. Must be 'parse_csv' or 'parse_json'
+    config_key: used when a file is on the cloud. If the file is on Azure store, the value is
+    "fs.azure.account.key.<storage-account-name>.blob.core.windows.net"
+    config_value: used when a file is on the cloud. If the file is on Azure store, the value is your account access key
+    """
+    def extract_data(self, filepath:str, parse_f, config_key:str, config_value:str):
+        self.spark.conf.set(config_key, config_value)
+        raw = self.spark.textFile(filepath)
+        parsed = raw.map(lambda line: parse_f(line))
+        data = self.spark.createDataFrame(parsed)
+        data.write.partitionBy('partition').mode('overwrite').parquet('output_dir')
---
+++ models/CommonEvent.py
@@ -0,0 +1,45 @@
+"""
+Data Model for CommonEvent
+"""
+__version__ = '0.1'
+__author__ = 'Dat Nguyen'
+
+from datetime import datetime
+
+
+class CommonEvent:
+    def __init__(self):
+        self.trade_dt = datetime.now()
+        self.rec_type = 'T'  # 'T' for Trade or 'Q' for Quote
+        self.symbol = ''
+        self.event_time = datetime.now()  # Event time
+        self.exchange = ''
+        self.event_seq_num = 0  # Event sequence number
+        self.arrival_time = datetime.now()  # Arrival time
+        self.trade_price = 0.0  # Trade price
+        self.trade_size = 0
+        self.bid_price = 0.0
+        self.bid_size = 0
+        self.ask_price = 0.0
+        self.ask_size = 0
+        self.partition = 'T'  # 'T' for Trade , 'Q' for Quote, or 'B' for bad record
+        self.original_line = ''  # used for bad record, keep the original record for checking
+
+    def __init__(self, partition, trade_dt, rec_type, symbol, event_time, event_seq_num, exchange, arrival_time,
+                 trade_price, trade_size, bid_price, bid_size, ask_price, ask_size,
+                 line):
+        self.partition = partition
+        self.trade_dt = trade_dt
+        self.rec_type = rec_type
+        self.symbol = symbol
+        self.event_time = event_time
+        self.event_seq_num = event_seq_num
+        self.exchange = exchange
+        self.arrival_time = arrival_time
+        self.trade_price = trade_price
+        self.trade_size = trade_size
+        self.bid_price = bid_price
+        self.bid_size = bid_size
+        self.ask_price = ask_price
+        self.ask_size = ask_size
+        self.original_line = line
