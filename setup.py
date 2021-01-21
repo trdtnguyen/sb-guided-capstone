a2bc900cfacabf132a143bba0ac64c0a18fad609
trdtnguyen <trauconnguyen@gmail.com>
2021-01-21 10:27:55 -0600

    add packing file

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="stock-analysis",
+    version="0.0.1",
+    author="Dat Nguyen",
+    author_email="trauconnguyen@gmail.com",
+    description="Stock prices analysis",
+    url="https://github.com/trdtnguyen/sb-guided-capstone",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6'
+
+)
